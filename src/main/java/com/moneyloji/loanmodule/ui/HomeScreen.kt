3e311c01b7610c74f0410f03a596e8efb8c9f9f1
Lokeshp1987 <40571880+Lokeshp1987@users.noreply.github.com>
2022-09-05 22:50:02 +0530

    myliberary
    myliberary

---
+++ src/main/java/com/moneyloji/loanmodule/ui/HomeScreen.kt
@@ -0,0 +1,12 @@
+package com.moneyloji.loanmodule.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.moneyloji.loanmodule.R
+
+class HomeScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_screen)
+    }
+}
